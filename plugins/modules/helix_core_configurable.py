1512652da9e9e8e80d240171fafa31074ad0de98
Asif Shaikh <ripclawffb@users.noreply.github.com>
2026-02-14 18:55:03 -0800

    chore: Dedupe connection documentation (#119)

--- plugins/modules/helix_core_configurable.py
+++ plugins/modules/helix_core_configurable.py
@@ -49,45 +49,15 @@ options:
             - The value of named configurable
         required: true
         type: str
-    server:
-        description:
-            - The hostname/ip and port of the server (perforce:1666)
-            - Can also use 'P4PORT' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4port
-    user:
-        description:
-            - A user with super user access
-            - Can also use 'P4USER' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4user
-    password:
-        description:
-            - The super user password
-            - Can also use 'P4PASSWD' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4passwd
-    charset:
-        default: none
-        description:
-            - Character set used for translation of unicode files
-            - Can also use 'P4CHARSET' environment variable
-        required: false
-        type: str
-        aliases:
-            - p4charset
     serverid:
         default: any
         description:
             - The server ID of the helix server
         required: false
         type: str
+
+extends_documentation_fragment:
+    - ripclawffb.helix_core.helix_core_connection
 
 author:
     - Asif Shaikh (@ripclawffb)
--- plugins/modules/helix_core_protect_info.py
+++ plugins/modules/helix_core_protect_info.py
@@ -31,39 +31,9 @@ seealso:
 
 
 options:
-    server:
-        description:
-            - The hostname/ip and port of the server (perforce:1666)
-            - Can also use 'P4PORT' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4port
-    user:
-        description:
-            - A user with super user access
-            - Can also use 'P4USER' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4user
-    password:
-        description:
-            - The super user password
-            - Can also use 'P4PASSWD' environment variable
-        required: true
-        type: str
-        aliases:
-            - p4passwd
-    charset:
-        default: none
-        description:
-            - Character set used for translation of unicode files
-            - Can also use 'P4CHARSET' environment variable
-        required: false
-        type: str
-        aliases:
-            - p4charset
+
+extends_documentation_fragment:
+    - ripclawffb.helix_core.helix_core_connection
 
 author:
     - Asif Shaikh (@ripclawffb)
